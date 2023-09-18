348776800cc7fae1cd6e0661e1e2986f8acf7494
zmiller7 <anonzak@users.noreply.github.com>
2023-09-17 19:00:51 -0500

    Adding old work to clean up PC

---
+++ C/colorText.c
@@ -0,0 +1,87 @@
+#include "colorText.h"
+
+//reset
+void reset(void) {
+	printf("\033[0m");
+}
+
+//normal black
+void black(void) {
+	printf("\033[0;30m");
+}
+
+//normal red
+void red(void) {
+	printf("\033[0;31m");
+}
+
+//normal green
+void green(void) {
+	printf("\033[0;32m");
+}
+
+//normal yellow
+void yellow(void) {
+	printf("\033[0;33m");
+}
+
+//normal blue
+void blue(void) {
+	printf("\033[0;34m");
+}
+
+//normal purple
+void purple(void) {
+	printf("\033[0;35m");
+}
+
+//normal cyan
+void cyan(void) {
+	printf("\033[0;36m");
+}
+
+//normal white
+void white(void) {
+	printf("\033[0;37m");
+}
+
+
+//bold black
+void black(void) {
+	printf("\033[1;30m");
+}
+
+//bold red
+void red(void) {
+	printf("\033[1;31m");
+}
+
+//bold green
+void green(void) {
+	printf("\033[1;32m");
+}
+
+//bold yellow
+void yellow(void) {
+	printf("\033[1;33m");
+}
+
+//bold blue
+void blue(void) {
+	printf("\033[1;34m");
+}
+
+//bold purple
+void purple(void) {
+	printf("\033[1;35m");
+}
+
+//bold cyan
+void cyan(void) {
+	printf("\033[1;36m");
+}
+
+//bold white
+void white(void) {
+	printf("\033[1;37m");
+}
---
+++ C/colorText.h
@@ -0,0 +1,59 @@
+#ifndef COLORTEXT_H_
+#define COLORTEXT_H_
+
+#include <stdio.h>
+
+//reset
+void reset(void);
+
+//normal black
+void black(void);
+
+//normal red
+void red(void);
+
+//normal green
+void green(void);
+
+//normal yellow
+void yellow(void);
+
+//normal blue
+void blue(void);
+
+//normal purple
+void purple(void);
+
+//normal cyan
+void cyan(void);
+
+//normal white
+void white(void);
+
+
+//bold black
+void black(void);
+
+//bold red
+void red(void);
+
+//bold green
+void green(void);
+
+//bold yellow
+void yellow(void);
+
+//bold blue
+void blue(void);
+
+//bold purple
+void purple(void);
+
+//bold cyan
+void cyan(void);
+
+//bold white
+void white(void);
+
+
+#endif		//end COLORTEXT_H_
